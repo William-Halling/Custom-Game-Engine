e0029d3e8c0a5ea4343e7dad107c66b8241f336b
Jinxed Nation William Halling <32233703@student.murdoch.edu.au>
2026-03-16 06:44:54 +0800

    updates

---
+++ Lab_4/AABB.cpp
@@ -0,0 +1,57 @@
+#include "AABB.h"
+
+namespace LabPhysics
+{
+    AABB::AABB(const glm::vec3& size)
+    {
+        glm::vec3 half = size * 0.5f;
+
+        min = -half;
+        max = half;
+    }
+
+
+    void AABB::UpdatePosition(const glm::vec3& position)
+    {
+        glm::vec3 half = (max - min) * 0.5f;
+
+        min = position - half;
+        max = position + half;
+    }
+
+
+    const glm::vec3& AABB::GetMin() const
+    {
+        return min;
+    }
+
+
+    const glm::vec3& AABB::GetMax() const
+    {
+        return max;
+    }
+
+
+    void AABB::SetMin(const glm::vec3& newMin)
+    {
+        min = newMin;
+    }
+
+
+    void AABB::SetMax(const glm::vec3& newMax)
+    {
+        max = newMax;
+    }
+
+
+    glm::vec3 AABB::GetSize() const
+    {
+        return max - min;
+    }
+
+
+    glm::vec3 AABB::GetCenter() const
+    {
+        return (min + max) * 0.5f;
+    }
+}
--- Lab_4/AABB.h
+++ Lab_4/AABB.h
@@ -1,32 +1,29 @@
 #pragma once
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
 namespace LabPhysics
 {
-	class AABB
-	{
-        public:
+	    class AABB
+    {
+    public:
 
-            glm::vec3 boxSize;
+        AABB(const glm::vec3& size = glm::vec3(1.0f));
 
+        void UpdatePosition(const glm::vec3& position);
 
-            glm::vec3 min;
-            glm::vec3 max;
+        const glm::vec3& GetMin() const;
+        const glm::vec3& GetMax() const;
 
+        void SetMin(const glm::vec3& min);
+        void SetMax(const glm::vec3& max);
 
-            // Constructor
-            AABB(const glm::vec3& boxSize = glm::vec3(1.0f, 1.0f, 1.0f))
-                : boxSize(boxSize), min(-boxSize * 0.5f), max(boxSize * 0.5f) {}
+        glm::vec3 GetSize() const;
+        glm::vec3 GetCenter() const;
 
-            void updateAABBPosition(glm::vec3 position)
-            {
-                min = position - boxSize * 0.5f;
-                max = position + boxSize * 0.5f;
-            }
+    private:
 
-        private:
-	};
-
+        glm::vec3 min;
+        glm::vec3 max;
+    };
 }
