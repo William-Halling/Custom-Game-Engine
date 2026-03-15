8aeda016f945bf0e851668cd27858281c8ce316e
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-03-16 07:28:42 +0800

    Update and rename Menu_Manager.h to MenuManager.h

---
+++ Lab_4/MenuManager.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <stack>
+#include <memory>
+
+class Menu;
+
+class MenuManager
+{
+	public:
+		MenuManager() = default;
+		~MenuManager() = default;
+
+		void pusheMenu(std::unique_ptr<Menu> menu);
+		void popMenu();
+
+		void draw();
+		bool hasMenu() const;
+
+	private:
+		std::stack<std::unique_ptr<Menu>> m_menuStack;
+};
