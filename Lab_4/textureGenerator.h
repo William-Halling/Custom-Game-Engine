3faddc880088a1c9d9d8bf2f9b55f4e67a1458c9
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-03-16 09:57:15 +0800

    Update textureGenerator.h

--- Lab_4/textureGenerator.h
+++ Lab_4/textureGenerator.h
@@ -1,39 +1,33 @@
 #pragma once
 
 #include "stb_image.h"
-
 #include <string>
-
 #include "image.h"
-
 #include <memory>
 
 
 
 class textureGenerator
 {
-
-
-
-public:
-
-	textureGenerator();
-	~textureGenerator();
-
-	bool addTexture(const char* filename);
-	int getNumTextures();
-	bool isLoaded(int index);
-	RGB getColor(int texNum, int x, int y);
-
-	bool setupNewTexture(int width, int height);
-	void setColor(int x, int y, RGB color);
-	unsigned char* getTex() { return newTex; }
-
-	float getRange();
-	float getTextureMax(int texNum);
-
-	unsigned int getTexWidth(int texNo);
-	unsigned int getTexHeight(int texNo);
+	public:
+	
+		textureGenerator();
+		~textureGenerator();
+	
+		bool addTexture(const char* filename);
+		int getNumTextures();
+		bool isLoaded(int index);
+		RGB getColor(int texNum, int x, int y);
+	
+		bool setupNewTexture(int width, int height);
+		void setColor(int x, int y, RGB color);
+		unsigned char* getTex() { return newTex; }
+	
+		float getRange();
+		float getTextureMax(int texNum);
+	
+		unsigned int getTexWidth(int texNo);
+		unsigned int getTexHeight(int texNo);
 
 
 	int getNewTexHeight()
