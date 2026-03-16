a932e6f6391a58d1111dd281539d59e52efdd8f3
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-03-16 10:48:15 +0800

    Update CustomerSupport_Command.cpp

--- Lab_4/CustomerSupport_Command.cpp
+++ Lab_4/CustomerSupport_Command.cpp
@@ -1,92 +1,47 @@
+// CustomerSupport_Command.cpp
 #include "CustomerSupport_Command.h"
+#include "BackCommand.h"
+#include <imgui.h>
 
-
-
-CustomerSupport_Command::CustomerSupport_Command()
-	: m_Details("Customer Support"),
-	m_ContactName("William Halling"),
-	m_ContactEmail("32233703@student.murdoch.edu.au"),
-	m_BackButton(std::make_shared<Back_Command>(Menu_Manager& menuManager))
+CustomerSupport_Command::CustomerSupport_Command(std::shared_ptr<BackCommand> backCmd)
+    : m_Details("Customer Support"),
+      m_ContactName("William Halling"),
+      m_ContactEmail("32233703@student.murdoch.edu.au"),
+      m_BackCommand(backCmd)
 {
-	addAvailability("Monday", "12:00 PM - 11:59 PM");
-	addAvailability("Tuesday", "12:00 PM - 11:59 PM");
-	addAvailability("Wednesday", "12.00 PM - 11:59 PM");
-	addAvailability("Thursday", "12:00 PM - 11:59 PM");
-	addAvailability("Friday", "12:00 PM - 11:59 PM");
+    m_Availability.push_back({"Monday", "12:00 PM - 11:59 PM"});
+    m_Availability.push_back({"Tuesday", "12:00 PM - 11:59 PM"});
+    m_Availability.push_back({"Wednesday", "12:00 PM - 11:59 PM"});
+    m_Availability.push_back({"Thursday", "12:00 PM - 11:59 PM"});
+    m_Availability.push_back({"Friday", "12:00 PM - 11:59 PM"});
 }
 
 
-std::string CustomerSupport_Command::getEmail() const
+
+void CustomerSupport_Command::Execute()
 {
-	return m_ContactEmail;
+    ShowWindow();
 }
 
 
-std::string CustomerSupport_Command::getName() const
+void CustomerSupport_Command::ShowWindow()
 {
-	return m_ContactName;
-}
+    ImGui::Begin("Customer Support");
+    ImGui::Text("Details: %s", m_Details.c_str());
+    ImGui::Text("Contact Name: %s", m_ContactName.c_str());
+    ImGui::Text("Contact Email: %s", m_ContactEmail.c_str());
+    ImGui::Text("Availability:");
 
-
-std::vector<CustomerSupport_Command::Availability> CustomerSupport_Command::getAvailability() const
-{
-	return m_Availability;
-}
-
-
-void CustomerSupport_Command::setEmail(const std::string& tempEmail)
-{
-	m_ContactEmail = tempEmail;
-}
-
-
-void CustomerSupport_Command::setName(const std::string& tempName)
-{
-	m_ContactName = tempName;
-}
-
-
-void CustomerSupport_Command::addAvailability(const std::string& day, const std::string& time)
-{
-	Availability availability;
-	availability.m_AvailableDays  = day;
-	availability.m_AvailableTimes = time;
-	m_Availability.push_back(availability);
-}
-
-
-void CustomerSupport_Command::displayCustomerSupportInfo() const
-{
-	bool m_DisplaySupport = true;
-
-	if (m_DisplaySupport) // Check if the flag is true to display the customer support info
-	{
-		ImGui::Begin("Customer Support");
-		ImGui::Text(("Details: " + m_Details).c_str());
-		ImGui::Text(("Contact Name: " + m_ContactName).c_str());
-		ImGui::Text(("Contact Email: " + m_ContactEmail).c_str());
-		ImGui::Text("Availability:");
-
-		for (const auto& availability : m_Availability)
-		{
-			ImGui::Text((availability.m_AvailableDays + ": " + availability.m_AvailableTimes).c_str());
-		}
-
-		if (ImGui::Button("Back")) // should somehow communicate with back_Command to go back to the core pause menu
-		{
-			m_DisplaySupport = false;
-			m_BackButton->executeTask();
-		}
-		ImGui::End();
-	}
-}
-
-
-void CustomerSupport_Command::executeTask()
-{
-	if (ImGui::Button("Customer Support"))
-	{
-		m_DisplaySupport = true;
-		displayCustomerSupportInfo();
-	}
+	
+	for (const auto& a : m_Availability)
+    {
+        ImGui::Text("%s: %s", a.day.c_str(), a.time.c_str());
+    }
+    
+	
+	if (ImGui::Button("Back") && m_BackCommand)
+    {
+        m_BackCommand->Execute();
+    }
+    ImGui::End();
 }
