17508723fafb0b28025350f1a38e99d4dfa87d9c
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-03-16 10:52:44 +0800

    Update EngGUI.cpp

--- Lab_4/EngGUI.cpp
+++ Lab_4/EngGUI.cpp
@@ -1,103 +1,72 @@
-
+// EngGUI.cpp
 #include "EngGUI.h"
 
-#include <GLFW/glfw3.h>
-#include "LabEngine.h"
-
-
-
-
-EngGUI::EngGUI()
+EngGUI::EngGUI(GLFWwindow* window)
+    : m_Window(window)
 {
-	m_PixelsGLFWWindow = LabEngine::getInstance().m_window->m_PixelsGLFWWindow;
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.IniFilename = nullptr; // disable .ini file
 
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	io.IniFilename = NULL;
+    ImGui::StyleColorsDark();
 
-	// Set ImGui style
-	ImGui::StyleColorsDark();
-
-	// Initialize ImGui GLFW and OpenGL bindings
-	ImGui_ImplGlfw_InitForOpenGL(m_PixelsGLFWWindow, true);
-	ImGui_ImplOpenGL3_Init("#version 330 core");
-
-
-	TextureFactory textFact;
-	// -- INIT PHOTO FOR GUI
-	auto groupPhoto = textFact.createTexture("group_photo.png");
-	groupPhoto->load();
-
-	image_texture_id = groupPhoto->texture;
-
+    ImGui_ImplGlfw_InitForOpenGL(m_Window, true);
+    ImGui_ImplOpenGL3_Init("#version 330 core");
 }
 
 
 EngGUI::~EngGUI()
 {
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 }
 
 
 void EngGUI::BeginFrame()
 {
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
 
-	//auto window = LabEngine::getInstance().m_window->window;
+    if (m_ShowDemo)
+    {
+        ImGui::SetNextWindowSize(ImVec2(300, 300));
+        ImGui::Begin("Welcome to our Demo!");
+        ImGui::Text("- Press M to open and close this manual!");
+        ImGui::Text("- Press K to go into wireframe mode!");
+        ImGui::Text("- Press X to quit this demo!");
+        ImGui::Text("- Press WASD keys to move!");
+        ImGui::Text("- Press R key to spawn boxes (test)");
+        ImGui::Text("- Press the F key to fly!");
+        ImGui::Text("- Use the mouse or arrow keys to look!");
+        ImGui::End();
 
-	ImGui_ImplOpenGL3_NewFrame();
-	ImGui_ImplGlfw_NewFrame();
-	ImGui::NewFrame();
-
-
-	if (show_demo_window) {
-		//ImGui::SetNextWindowSize(ImVec2(300, 200)); // Set the window size 
-		ImGui::SetNextWindowSize(ImVec2(300, 300)); // Set the window size
-
-		// ImGui content goes here, e.g.:
-		ImGui::Begin("Welcome to our Demo! (pls have mercy)");
-		ImGui::Text("- Press M to open and close this manual!");
-		ImGui::Text("- Press K to go into wireframe mode!");
-		ImGui::Text("- Press X to quit this demo!");
-		ImGui::Text("- Press WASD keys to move!");
-		ImGui::Text("- Press R key to spawn boxes (test)");
-		ImGui::Text("- Press the F key to fly!");
-		ImGui::Text("- Use the mouse or arrow keys to look!");
-		ImGui::End();
-
-		if (show_image) {
-			glfwSetInputMode(m_PixelsGLFWWindow, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-			//ImVec2 image_size(groupPhoto->width, groupPhoto->height); // Set the image display size to 300x200
-			ImVec2 image_size(1000, 700); // Set the image display size to 300x200
-			ImGui::Begin("Image Display");
-			ImGui::Image((void*)(intptr_t)image_texture_id, image_size, ImVec2(0, 1), ImVec2(1, 0));
-			if (ImGui::IsItemClicked()) {
-				glfwSetWindowShouldClose(m_PixelsGLFWWindow, true);
+		
+        if (m_ShowImage && m_GroupPhotoTex)
+        {
+            glfwSetInputMode(m_Window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+            ImGui::Begin("Image Display");
+            ImGui::Image((void*)(intptr_t)m_GroupPhotoTex, ImVec2(1000, 700), ImVec2(0,1), ImVec2(1,0));
+           
+			
+			if (ImGui::IsItemClicked())
+			{
+                glfwSetWindowShouldClose(m_Window, true);
 			}
 			ImGui::End();
-		}
-
-
-	}
-
+			
+        }
+    }
 }
 
 
-
-void EngGUI::EndFrame() {
-
-	ImGui::Render();
-
-}
-
-void EngGUI::renderData()
+void EngGUI::EndFrame()
 {
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData()); // doing this at the end so draws on top of everything else
+    ImGui::Render();
 }
-
-
-void EngGUI::MainMenu(){
-
+void EngGUI::Render()
+{
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
