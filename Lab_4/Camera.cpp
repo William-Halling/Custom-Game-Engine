3b603db3a9f3ee5c11144ec30b70d26ad41f96f8
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-03-16 10:46:24 +0800

    Update Camera.cpp

--- Lab_4/Camera.cpp
+++ Lab_4/Camera.cpp
@@ -1,112 +1,90 @@
+// Camera.cpp
 #include "Camera.h"
 
-
-Camera::Camera(const glm::vec3& initialPosition) : m_Location(initialPosition)
+Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch)
+    : m_Location(position),
+      m_WorldUp(up),
+      m_Yaw(yaw),
+      m_Pitch(pitch),
+      m_Zoom(Zoom) // use default Zoom from options
 {
-
-    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-    float yaw = YAW;
-    float pitch = PITCH;
-
-
-    m_Direction = glm::vec3(0.0f, 0.0f, -1.0f);
-
-
-    MovementSpeed = SPEED;
-    MouseSensitivity = SENSITIVITY;
-    Zoom = ZOOM;
-
-    m_Location = initialPosition;
-    m_VerticalPlane = up;
-    m_CameraYaw = yaw;
-    m_CameraPitch = pitch;
-
-    updateCameraVectors();
+    updateVectors();
 }
 
 
-Camera::Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : m_Direction(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+void Camera::ProcessKeyboard(Camera_Movement direction, float deltaTime)
 {
-    m_Location      = position;
-    m_VerticalPlane = up;
-    m_CameraYaw     = yaw;
-    m_CameraPitch   = pitch;
-    updateCameraVectors();
+    float velocity = MovementSpeed * deltaTime;
+    if (direction == FORWARD)
+    {
+        m_Location += m_Direction * velocity;
+    }
+    
+    if (direction == BACKWARD)
+    { 
+        m_Location -= m_Direction * velocity;
+    }
+    
+    if (direction == LEFT)
+    {
+        m_Location -= m_Right * velocity;
+    }
+    
+    if (direction == RIGHT)
+    {
+        m_Location += m_Right * velocity;
+    }
 }
 
 
-
-Camera::Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch)
+void Camera::ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch)
 {
-    m_Location       = glm::vec3(posX, posY, posZ);
-    m_VerticalPlane  = glm::vec3(upX, upY, upZ);
-    m_CameraYaw      = yaw;
-    m_CameraPitch    = pitch;
-    updateCameraVectors();
-}
+    xoffset *= MouseSensitivity;
+    yoffset *= MouseSensitivity;
 
+    m_Yaw   += xoffset;
+    m_Pitch += yoffset;
 
-Camera::~Camera() {}
-
-
-
-const glm::vec3& Camera::getCameraLocation() const { return m_Location;        }
-glm::vec3 Camera::getCameraDirection()             { return m_Direction;       }
-glm::vec3 Camera::getHorizontalPlane()       const { return m_HorizontalPlane; }
-glm::vec3 Camera::getVerticalPlane()         const { return m_VerticalPlane;   }
-float     Camera::getRotationSpeed()         const { return m_RotationSpeed;   }
-float     Camera::getCameraPitch()           const { return m_CameraPitch;     }
-float     Camera::getCameraZoom()            const { return m_CameraZoom;      }
-float     Camera::getCameraYaw()             const { return m_CameraYaw;       }
-glm::mat4 Camera::GetViewMatrix() { return glm::lookAt(m_Location, m_Location + m_Direction, m_VerticalPlane); }
-
-
-void Camera::setCameraLocation(const glm::vec3& tempLocation)         { m_Location        = tempLocation;        }
-void Camera::setCameraDirection(const glm::vec3& tempDirection)       { m_Direction       = tempDirection;       }
-void Camera::setHorizontalPlane(const glm::vec3& tempHorizontalPlane) { m_HorizontalPlane = tempHorizontalPlane; }
-void Camera::setVerticalPlane(const glm::vec3& tempVerticalPlane)     { m_VerticalPlane   = tempVerticalPlane;   }
-void Camera::setRotationSpeed(float tempRotationSpeed)                { m_RotationSpeed   = tempRotationSpeed;   }
-void Camera::setCameraPitch(float tempPitch)                          { m_CameraPitch     = tempPitch;           }
-void Camera::setCameraZoom(float tempZoom)                            { m_CameraZoom      = tempZoom;            }
-void Camera::setCameraYaw(float tempYaw)                              { m_CameraYaw       = tempYaw;             }
-
-
-
-void Camera::readMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch)
-{
-    float sensitivity = 0.1f;
-    xoffset *= sensitivity;
-    yoffset *= sensitivity;
-
-
-    m_CameraYaw += xoffset;
-    m_CameraPitch += yoffset;
-
-
+    
     if (constrainPitch)
     {
-        if (m_CameraPitch > 89.0f)
+        if (m_Pitch > 89.0f)
         {
-            m_CameraPitch = 89.0f;
+            m_Pitch = 89.0f;
         }
-
-        if (m_CameraPitch < -89.0f)
+        
+        if (m_Pitch < -89.0f)
         {
-            m_CameraPitch = -89.0f;
+            m_Pitch = -89.0f;
         }
     }
-    updateCameraVectors();
+    updateVectors();
 }
 
 
-void Camera::updateCameraVectors()
+void Camera::ProcessMouseScroll(float yoffset)
+{
+    Zoom -= yoffset;
+    if (Zoom < 1.0f)
+    {
+        Zoom = 1.0f;
+    }
+
+    if (Zoom > 45.0f) 
+    {
+        Zoom = 45.0f;
+    }
+}
+
+
+void Camera::updateVectors()
 {
     glm::vec3 front;
-    front.x = cos(glm::radians(m_CameraYaw)) * cos(glm::radians(m_CameraPitch));
-    front.y = sin(glm::radians(m_CameraPitch));
-    front.z = sin(glm::radians(m_CameraYaw)) * cos(glm::radians(m_CameraPitch));
+    front.x = cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+    front.y = sin(glm::radians(m_Pitch));
+    front.z = sin(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
     m_Direction = glm::normalize(front);
 
-
-    m_HorizontalPlane = glm::normalize(glm::cross(m_Direction, m_VerticalPlane));
+    m_Right = glm::normalize(glm::cross(m_Direction, m_WorldUp));
+    m_Up    = glm::normalize(glm::cross(m_Right, m_Direction));
 }
