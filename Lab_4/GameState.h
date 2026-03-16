39aba0b35af08dcd7bc2f800dbad6fb2fa4197d3
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-03-16 10:54:33 +0800

    Update GameState.h

--- Lab_4/GameState.h
+++ Lab_4/GameState.h
@@ -1,37 +1,14 @@
+// GameState.h
 #pragma once
-
-#include <map>
 #include <memory>
 
-class Window {};
-class Menu_Commands {};
-
-class Mouse_Input {};
-class Keyboard_Input {};
-class EngGUI {};
-class ResumeCommand : public Menu_Commands {};
-class SaveCommand : public Menu_Commands {};
-class UI_Manager {};
-
-
-/***************************************************************************************************************
- * @brief : IGameState
- * @brief :
- *
- *
- *
- * @date    : May 2023
- * @author  : William Halling
- * @version : 1.0
- ******************************************************************************************************************/
-
-class GameState 
+class GameState
 {
-    public:
-        virtual ~GameState() = default;
-        virtual void handlePlayerInput() = 0;
-        virtual void Enter()    = 0;
-        virtual void generate() = 0;
-        virtual void update()   = 0;
-        virtual void Exit()     = 0;
+public:
+    virtual ~GameState() = default;
+    virtual void Enter() = 0;
+    virtual void Exit() = 0;
+    virtual void HandleInput() = 0;
+    virtual void Update(float deltaTime) = 0;
+    virtual void Render() = 0;
 };
